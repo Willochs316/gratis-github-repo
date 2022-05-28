93bdcefd07d4ac086a3bb2fdb13cc9b30a9c2288
Willochs316 <willochs316@gmail.com>
2022-05-28 07:04:26 +0100

    Added changes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import UserItems from './Components/Users/UserItems';
 import axios from 'axios';
-import { USER_PER_PAGE } from './Utils/Constants';
 
 const App = () => {
-  const [items, setItems] = useState([]);
+  const [githubResponse, setGithubResponse] = useState({
+    total_count: 0,
+    items: [],
+    incomplete_results: false,
+  });
   const [isLoading, setIsLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
 
-  console.log(totalPages, 'pagessss 101');
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(
-        `https://api.github.com/search/repositories?q=created:%3E2021-08-13&sort=stars&order=desc/github`
+      currentPage === 1 && setIsLoading(true);
+      const { data } = await axios(
+        `https://api.github.com/search/repositories?q=created:%3E2021-08-13&sort=stars&order=desc/github&page=${currentPage}`
       );
-
-      console.log(result.data, 'data logged');
-      setItems(result.data);
-      setIsLoading(false);
-
-      setTotalPages(Math.ceil(result.data.items.length / USER_PER_PAGE));
+      setGithubResponse((prev) => ({
+        ...prev,
+        ...data,
+      }));
+      currentPage === 1 && setIsLoading(false);
     };
     fetchItems();
-  }, []);
+  }, [currentPage]);
 
-  const handleClick = (num) => {
-    setCurrentPage(num);
+  const loadMore = () => {
+    githubResponse.incomplete_results && setCurrentPage((prev) => (prev += 1));
   };
 
   return (
     <UserItems
       isLoading={isLoading}
-      items={items}
+      githubResponse={githubResponse}
       currentPage={currentPage}
       totalPages={totalPages}
-      handleClick={handleClick}
+      loadMore={loadMore}
     />
   );
 };
--- src/Components/Users/UserItems.js
+++ src/Components/Users/UserItems.js
@@ -1,117 +1,76 @@
 import React from 'react';
-import UserIcons from '../Commons/Icons';
-import {
-  FaHome,
-  FaRegArrowAltCircleLeft,
-  FaRegArrowAltCircleRight,
-  FaSearch,
-  FaTimes,
-} from 'react-icons/fa';
-import { USER_PER_PAGE } from '../../../src/Utils/Constants';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Button from '../Commons/Button';
 import Input from '../Commons/Input';
 import './User.css';
-import Pagination from '../../Components/Pagination/Pagination';
+import CircularProgress from '@mui/material/CircularProgress';
 
-const UserItems = ({
-  items,
-  currentPage,
-  isLoading,
-  totalPages,
-  handleClick,
-}) => {
-  console.log(USER_PER_PAGE, 'my ONE users items logged here');
+const UserItems = ({ githubResponse, currentPage, isLoading, loadMore }) => {
 
-  const startIndex = (currentPage - 1) * USER_PER_PAGE;
-  const selectedUsers = items?.items?.slice(
-    startIndex,
-    startIndex + USER_PER_PAGE
-  );
+  const onNext = () => {
+    loadMore?.();
+  };
+
 
   return isLoading ? (
-    <h1>Loading...</h1>
+    <CircularProgress />
   ) : (
     <div className='container mt-3'>
-      <div className='navbar'>
-        <div className='header'>
-          <h1 className='h1-text'>Trending Repos</h1>
-        </div>
-        <div className='nav-contents'>
-          <div className='icons-container'>
-            <UserIcons icons={FaRegArrowAltCircleLeft} className='fa-chevro' />
-            <UserIcons icons={FaRegArrowAltCircleRight} className='fa-chevro' />
-            <UserIcons icons={FaTimes} className='fa-chevro' />
-            <UserIcons icons={FaHome} className='fa-chevro' />
-          </div>
-
-          <div className='input-container'>
-            <Input className='trend-input' />
-            <button className='btn-search'>
-              <UserIcons icons={FaSearch} className='fa-chevr' />
-            </button>
-          </div>
-        </div>
+      <div className='input-container'>
+        <Input className='em-input' placeholder='Search....' />
+        <Button className='input-button' title='Search' />
       </div>
 
       <div className='items-container'>
-        {selectedUsers.map((item) => (
-          <div className='main-container' key={item.id}>
-            <div className='profile-repo'>
-              <div className='repo-image'>
-                <img
-                  src={item.owner.avatar_url}
-                  style={{ height: '100px', width: '100px' }}
-                  alt={item.owner.name}
-                />
-              </div>
+        <InfiniteScroll
+          dataLength={githubResponse?.items}
+          next={onNext}
+          hasMore={githubResponse?.incomplete_results}
+          loader={<CircularProgress />}
+        >
+          {githubResponse?.items?.map((item) => (
+            <div className='main-container' key={item.id}>
+              <div className='profile-repo'>
+                <div className='repo-image-container'>
+                  <img
+                    src={item.owner.avatar_url}
+                    className='repo-image'
+                    alt={item.owner.name}
+                  />
+                </div>
 
-              <div className='repo-details'>
-                <div className='rep-content'>
-                  <div
-                    style={{
-                      width: '100%',
-                      display: 'flex',
-                      justifyContent: 'flex-start',
-                      alignItems: 'center',
-                    }}
-                  >
-                    <h1 style={{ fontSize: '24px' }}>{item.full_name}</h1>
+                <div className='repo-details'>
+                  <div className='repo-header'>
+                    <h1 className='repo-1'>{item.full_name}</h1>
                   </div>
 
-                  <div
-                    style={{
-                      width: '100%',
-                      display: 'flex',
-                      justifyContent: 'flex-start',
-                      alignItems: 'center',
-                    }}
-                  >
-                    <p style={{ fontSize: '16px' }}>
-                      An open Source Machine Learning framework for everyone
+                  <div className='repo-text'>
+                    <p className='repo-p1'>
+                      {item.description}
                       <span> {item.url}</span>
                     </p>
                   </div>
-                </div>
 
-                <div className='rate'>
-                  <div className='rate-1'>Stars: {item.stargazers_count}k</div>
-                  <div className='rate-2'>
-                    Issues: {item.open_issues_count}k
-                  </div>
+                  <div className='rate'>
+                    <div className='rate-1'>
+                      Stars: {item.stargazers_count}k
+                    </div>
 
-                  <div className='rate-text-content'>
-                    <span className='rate-text'>
-                      Submiitted 30 days ago by {item.name}
-                    </span>
+                    <div className='rate-2'>
+                      Issues: {item.open_issues_count}k
+                    </div>
+
+                    <div className='rate-text-content'>
+                      <span className='rate-text'>
+                        Submiitted 30 days ago by {item.name}
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
-
-      <div className='footer'>
-        <Pagination totalPages={totalPages} handleClick={handleClick} />
+          ))}
+        </InfiniteScroll>
       </div>
     </div>
   );
